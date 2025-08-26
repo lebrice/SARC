8ec188adcff908084b497bcb5b7c0fe11f4bc30a
Fabrice Normandin <normandf@mila.quebec>
2025-08-26 14:55:59 -0400

    Hard-code list of core profs for now
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- examples/ivado_usage_query.py
+++ examples/ivado_usage_query.py
@@ -1,5 +1,7 @@
 import functools
 from datetime import datetime
+from pathlib import Path
+from typing import Iterable, Sequence
 
 import pandas as pd
 import plotly.express as px
@@ -11,6 +13,8 @@ from examples.waste_monitor.sarc_patches import (
 )
 from sarc.client.users.api import User, get_users
 from sarc.config import MTL
+
+CORE_PROF_EMAILS = Path("core_profs.txt").read_text().splitlines()
 
 
 @functools.cache
@@ -56,6 +60,23 @@ def get_group_students(
     )
 
 
+def is_prof(user: User, all_users: Iterable[User]) -> bool:
+    """A user is considered a professor if they don't have a supervisor (and no co-supervisor) and at least one student."""
+    if any(user.mila_ldap.get(k) for k in ["supervisor", "co_supervisor"]):
+        return False
+    user_email = user.mila.email
+    return any(
+        other_user.mila_ldap.get("supervisor") == user_email
+        or other_user.mila_ldap.get("co_supervisor") == user_email
+        for other_user in all_users
+    )
+
+
+def is_core_prof(user: User, all_users: Iterable[User]) -> bool:
+    """A user is considered a professor if they don't have a supervisor (and no co-supervisor) and at least one student."""
+    return is_prof(user, all_users) and user.mila.email in CORE_PROF_EMAILS
+
+
 def main():
     _setup_logging(verbose=1)
     # 1er avril 2025 au 31 juillet 2025
@@ -69,37 +90,116 @@ def main():
         for user in users_in_period
         if (supervisor := user.mila_ldap.get(key))
     )
+    professors = [
+        user
+        for user in users_in_period
+        if user.mila is not None and user.mila.email in all_profs_emails
+    ]
     sarc_data = get_clean_sarc_data(filter)
 
     # Simplifying for now: attributing all the compute usage to only the supervisor instead of:
     # a) splitting between supervisor and co-supervisor, or
     # b) duplicating the values by counting it for both the supervisor or co-supervisor.
+    usage_data, fig = usage_plot_mila_or_drac(
+        filter, users_in_period, professors, sarc_data
+    )
 
-    usage_per_prof_on_mila_cluster = (
-        sarc_data.query("cluster_name=='mila'")
-        .groupby("user.mila_ldap.supervisor")[
+    fig.show(renderer="browser")
+    fig.write_image("usage_per_prof_on_mila_cluster.png")
+    # plt.show(block=True)
+    print(usage_data.to_markdown())
+
+
+def usage_plot_mila_or_drac(
+    filter: FilteringOptions,
+    users_in_period: Sequence[User],
+    professors: Sequence[User],
+    sarc_data: pd.DataFrame,
+    mila_only: bool = True,
+    drac_only: bool = False,
+):
+    sarc_data = sarc_data.copy()
+    if mila_only:
+        sarc_data = sarc_data.query("cluster_name=='mila'")
+    elif drac_only:
+        sarc_data = sarc_data.query("cluster_name!='mila'")
+        sarc_data = sarc_data[
+            ~sarc_data["cluster_name"].isin(["tamia", "killarney", "vulcan"])
+        ]
+    else:
+        # PAICE only.
+        sarc_data = sarc_data[
+            sarc_data["cluster_name"].isin(["tamia", "killarney", "vulcan"])
+        ]
+    # For jobs where users don't have a supervisor, and the user itself is not a prof, we set the "supervisor" to "Staff/Industry/Other
+    prof_emails = {prof.mila.email for prof in professors}
+
+    is_prof = sarc_data["user.mila.email"].isin(prof_emails)
+    is_staff = sarc_data["user.mila_ldap.supervisor"].isna() & ~is_prof
+    # sarc_data.loc[is_staff, "user.mila_ldap.supervisor"] = "Staff/Industry/Other"
+
+    # TODO: Need to count the compute of profs towards themselves, even though they don't have a supervisor field!
+    usage_data = (
+        sarc_data.groupby("user.mila_ldap.supervisor", dropna=False)[
             ["gpu_cost", "gpu_equivalent_cost", "rgu_cost", "rgu_equivalent_cost"]
         ]
         .sum()
         .div(pd.Timedelta(days=1))  # convert from datetime to float (gpu/rgu days).
-        # .plot.pie(
-        #     # y="rgu_equivalent_cost",
-        #     subplots=True,
-        #     figsize=(12, 8),
-        #     legend=False,
-        #     title="Usage on mila cluster",
-        # )
+    )
+    # TODO: renaming nan in index to something else doesn't seem to work.
+    # usage_data = usage_data.rename(index={np.nan: "Staff/Industry/Other"})
+
+    usage_data = pd.concat(
+        [
+            usage_data[usage_data["user.mila_ldap.supervisor"].isin(CORE_PROF_EMAILS)],
+            pd.DataFrame(
+                [
+                    usage_data[
+                        usage_data["user.mila_ldap.supervisor"].notna()
+                        & usage_data["user.mila_ldap.supervisor"].isin(CORE_PROF_EMAILS)
+                    ]
+                ],
+                index=["Non-core profs"],
+                columns=usage_data.columns,
+            ),
+            pd.DataFrame(
+                [usage_data[usage_data["user.mila_ldap.supervisor"].isna()]],
+                index=["Staff/Industry/Other"],
+                columns=usage_data.columns,
+            ),
+        ]
+    )
+
+    usage_data = usage_data.reset_index()
+    usage_data = usage_data.assign(
+        core=usage_data["user.mila_ldap.supervisor"].isin(CORE_PROF_EMAILS)
+    )
+    usage_data = usage_data.assign(staff=is_staff)
+
+    total_for_non_core_profs = usage_data[
+        usage_data["core"].notna() & ~(usage_data["core"].fillna(True, inplace=False))
+    ].sum()
+
+    usage_data = pd.concat(
+        [
+            usage_data[
+                usage_data["core"] | usage_data["core"].isna()
+            ],  # core profs and staff/other
+            pd.DataFrame(  # add a single entry for all non-core profs
+                [total_for_non_core_profs],
+                index=["Non-core profs"],
+                columns=usage_data.columns,
+            ),
+        ],
     )
     fig = px.pie(
-        usage_per_prof_on_mila_cluster,
+        usage_data,
         values="rgu_equivalent_cost",
-        names=usage_per_prof_on_mila_cluster.index.values,
-        title=f"Usage on mila cluster between {filter.start.date()} and {filter.end.date()}",
+        names=usage_data.index.values,
+        title=f"Usage on {'Mila' if mila_only else 'DRAC' if drac_only else 'PAICE'} cluster between {filter.start.date()} and {filter.end.date()}",
     )
-    fig.show(renderer="browser")
-    fig.write_image("usage_per_prof_on_mila_cluster.png")
-    # plt.show(block=True)
-    print(usage_per_prof_on_mila_cluster.to_markdown())
+
+    return usage_data, fig
     # sarc_data =
 
 
